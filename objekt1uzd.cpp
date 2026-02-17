84e94ff75bdc341d81d226cd826ba61677f85659
ditassimoliunas-prog <ditas.simoliunas@gmail.com>
2026-02-17 19:40:17 +0200

    Pridetas meniu

--- objekt1uzd.cpp
+++ objekt1uzd.cpp
@@ -14,9 +14,10 @@ using std::setw;
 using std::endl;
 using std::setprecision;
 using std::sort;
+using std::fixed;
 struct Studentas {
     string vardas = "A", pavarde = "BB";
-    //int paz jei dinaminis masyvas
+    //int  *paz jei dinaminis masyvas
     vector <int> paz;
     int egz;
     double rez;
@@ -40,14 +41,56 @@ double mediana(vector <int> paz) {
 
 void inputas(vector<Studentas>& grupe);
 void outputas(const vector<Studentas>& grupe);
+void menu();
 int main() {
+    menu();
+    return 0;
+}
+
+void menu() {
     vector<Studentas>grupe;
-    inputas(grupe);
-    outputas(grupe);
+    int pas; //pasirinkimas
+    bool testi = true;
+
+    while (testi) {
+        cout << "----------------------------------------------" << endl;
+        cout << " Studentu Rezultatu skaiciavimo aplikacija " << endl;
+        cout << "----------------------------------------------" << endl;
+        cout << "1. Ivesti duomenis ranka " << endl;
+        cout << "2. Generuoti tik pazymius " << endl;
+        cout << "3. Generuoti studentu vardus, pavardes ir pazymius " << endl;
+        cout << "4. Baigti darba " << endl;
+        cin >> pas;
+
+        switch (pas) {
+        case 1:
+            cout << "1. Ivesti duomenis ranka " << endl;
+            inputas(grupe);
+            break;
+        case 2:
+            cout << "2. Generuoti tik pazymius " << endl;
+            cout << "Funkcija dar nesukurta " << endl;
+            break;
+        case 3:
+            cout << "3. Generuoti studentu vardus, pavardes ir pazymius " << endl;
+            cout << "Funkcija dar nesukurta " << endl;
+            break;
+        case 4:
+            cout << "Programa uzdaroma " << endl;
+            testi = false;
+            break;
+        default:
+            cout << "Klaida! Pasirinkite skaiciu nuo 1 iki 4 " << endl;
+            cin.clear();
+            cin.ignore(1000, '\n');
+            break;
+        }
+    }
 }
 void inputas(vector<Studentas>& grupe) {
     for (int ii = 0; ii < 2; ii++) {
         Studentas A;
+
         cout << "Iveskite varda ir pavarde: ";
         cin >> A.vardas >> A.pavarde;
         cout << "----------------------------------------------" << endl;
@@ -82,10 +125,10 @@ void inputas(vector<Studentas>& grupe) {
 
 void outputas(const vector <Studentas>& grupe) {
     for (auto A : grupe) {
-        cout << left << setw(10) << "Vardas " << left << setw(20) << "Pavarde " << left << setw(30) << "Galutinis (Vid.) " << left << setw(100) << "Galutinis (Med.) " << endl;
+        cout << left << setw(10) << "Vardas " << left << setw(20) << "Pavarde " << left << setw(30) << "Galutinis (Vid.) " << left << setw(40) << "Galutinis (Med.) " << endl;
         cout << left << setw(10) << A.vardas << left << setw(20) << A.pavarde;
         //for (auto k : A.paz) cout << setw(3) << k;
         //cout << setw(5) << A.egz;
-        cout << left << setw(10) << setprecision(2) << A.rez << left << setw(40) << setprecision(2) << A.med << endl;
+        cout << left << setw(30) << fixed << setprecision(2) << A.rez << left << setw(40) << fixed << setprecision(2) << A.med << endl;
     }
 }
