c79214044bbb4832a25c8b5f6902c3bfd1569959
ditassimoliunas-prog <ditas.simoliunas@gmail.com>
2026-03-05 19:02:50 +0200

    Sukurti du nauji header failai, input.h ir studentas.h

---
+++ header_files/Studentas.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <string>
+#include <vector>
+
+using std::string;
+using std::vector;
+
+struct Studentas {
+    string vardas = "A";
+    string pavarde = "BB";
+    vector<int> paz;
+    int egz;
+    double rez;
+    double med;
+};
