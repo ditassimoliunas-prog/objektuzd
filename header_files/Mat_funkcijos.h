46feb6632e165d0e3bac07bc357062c7b94a6b2e
ditassimoliunas-prog <ditas.simoliunas@gmail.com>
2026-03-27 21:11:08 +0200

    Keli mazi pakeitimai

--- header_files/Mat_funkcijos.h
+++ header_files/Mat_funkcijos.h
@@ -8,7 +8,7 @@ using std::vector;
 
 bool arTikRaides(const string& str);
 double mediana(vector <int> paz);
-double vidurkis(const vector<int>& paz);
+double vidurkis(const vector<int>& paz); // Priima per referencą greičiui
 double galutinisBalas(double vidMed, int egz);
 int generuotiPazymi();
 int generuotiEgzamina();
