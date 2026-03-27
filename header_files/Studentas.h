792db538555be2236fd470a6f6f50cd62c32b634
ditassimoliunas-prog <ditas.simoliunas@gmail.com>
2026-03-27 21:09:08 +0200

    Pridėta pradinių reikšmių inicializacija Studentas struktūroje

--- header_files/Studentas.h
+++ header_files/Studentas.h
@@ -10,7 +10,7 @@ struct Studentas {
     string vardas = "A";
     string pavarde = "BB";
     vector<int> paz;
-    int egz;
-    double rez;
-    double med;
+    int egz = 0; // Default inicializacija
+    double rez = 0.0; // Pradinis balų priskyrimas
+    double med = 0.0;
 };
